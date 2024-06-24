0a2dc14d34273b3ffa88c7c696706a317644b3ae
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-06-23 21:29:03 -0700

    feat(plugin): :sparkles: expose more module apis

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const obsidian = require("obsidian")
-const electron = require("electron").remote
 const Module = require("module")
+const path = require("path")
 
-if (!String.prototype.format) {
+if (!String.prototype.format || typeof String.prototype.format !== 'function') {
   String.prototype.format = function() {
     var args = arguments;
     return this.replace(/{(\d+)}/g, function(match, number) { 
@@ -14,6 +14,14 @@ if (!String.prototype.format) {
   };
 }
 
+if(!String.isNullOrWhitespace || typeof String.isNullOrWhitespace !== 'function'){
+  String.isNullOrWhitespace = function( str ){
+    if( typeof str !== 'string')
+      str = str.toString();
+    return str == null || str.trim() === ''
+  }
+}
+
 const internals = {}
 /*
 class APISettingsTab extends obsidian.PluginSettingTab {
@@ -21,34 +29,76 @@ class APISettingsTab extends obsidian.PluginSettingTab {
 }
 */
 
+class ModulesAPI {
+  constructor( r = require, m = globalThis.module ){
+    this.require = r
+    
+    if( String.isNullOrWhitespace( process.env.NODE_PATH ) )
+      m.paths.push( process.env.NODE_PATH );
+
+    this.getSearchPaths = ( dir = "{0}") => {
+      const paths = Module._nodeModulePaths( dir )
+      if( String.isNullOrWhitespace( dir ) )
+          return paths
+      paths.push.apply( paths, m.paths)
+      return paths.map( path => path.format( dir ) )
+    }
+
+    Object.defineProperties( this, {
+      "paths": {
+        get() {
+          this.getSearchPaths("")
+        },
+        set( dir ){
+          m.paths.push( dir )
+        }
+      },
+      "cache": {
+        get() {
+          return this.require.cache
+        }
+      }
+    })
+  }
+}
+
 class APIPlugin extends obsidian.Plugin {
-    async onload(){
-        // await this.loadSettings()
-        // this.addSettingTab(new APISettingsTab(this.app, this))
-        // const { settings } = this
-        internals.plugin = this
-        /*
-        this.addCommand({
-            id: "command-id",
-            name: "command name"
-            callback: cb
-        })
-        */
-    }
-    // onunload(){}
-    // async loadSettings(){}
-    // async saveSettings(){}
+  constructor(){
+    super()
 
-    require = require
-    obsidian = obsidian
-    electron = electron
+    Object.defineProperties( this, {
+      "obsidian": {
+        get() {
+          return this.module.require("obsidian")
+        }
+      },
+      "electron": {
+        get() {
+          return this.module.require("electron")
+        }
+      }
+    })
+  }
+  async onload(){
+    // await this.loadSettings()
+    // this.addSettingTab(new APISettingsTab(this.app, this))
+    // const { settings } = this
+    internals.plugin = this
+    /*
+    this.addCommand({
+        id: "command-id",
+        name: "command name"
+        callback: cb
+    })
+    */
+  }
+  // onunload(){}
+  // async loadSettings(){}
+  // async saveSettings(){}
 
-    getSearchPaths( dir ){
-        const paths = Module._nodeModulePaths("{0}")
-        if( dir == null || dir.trim() === '' )
-            return paths
-        return paths.map( path => path.format( dir ) )
-    }
+  ModulesAPI = ModulesAPI
+
+  module = new ModulesAPI()
 }
 
 module.exports = APIPlugin
