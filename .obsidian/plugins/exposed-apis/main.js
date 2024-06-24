823de722b3df4715f7b3d6927853eb0a5a1d16d1
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-06-23 18:58:34 -0700

    test(plugin): :adhesive_bandage: fix failing test

--- .obsidian/plugins/exposed-apis/main.js
+++ .obsidian/plugins/exposed-apis/main.js
@@ -1,1 +1,52 @@
-let test;
+const obsidian = require("obsidian")
+const electron = require("electron").remote
+const Module = require("module")
+
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+const internals = {}
+/*
+class APISettingsTab extends obsidian.PluginSettingTab {
+    
+}
+*/
+
+class APIPlugin extends obsidian.Plugin {
+    async onload(){
+        // await this.loadSettings()
+        // this.addSettingTab(new APISettingsTab(this.app, this))
+        // const { settings } = this
+        internals.plugin = this
+        /*
+        this.addCommand({
+            id: "command-id",
+            name: "command name"
+            callback: cb
+        })
+        */
+    }
+    // onunload(){}
+    // async loadSettings(){}
+    // async saveSettings(){}
+
+    require = require
+    obsidian = obsidian
+    electron = electron
+
+    getSearchPaths( dir ){
+        const paths = Module._nodeModulePaths("{0}")
+        if( dir == null || dir.trim() === '' )
+            return paths
+        return paths.map( path => path.format( dir ) )
+    }
+}
